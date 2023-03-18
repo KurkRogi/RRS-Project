2d5e1afd3f7de0c4cba38454d936eb089b1ff66b
KurkRogi <igor.k.kruk@gmail.com>
2023-03-18 20:32:15 +0000

    Add working recognision of development status

--- RRS/settings.py
+++ RRS/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEV_ENV')
+DEBUG = os.environ.get('DEV_ENV') == 'True'
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.append(os.environ.get('HOST_NAME'))
